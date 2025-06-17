3a900c977c743381113e9aa71e0f77966d69336d
Anajulinhathomaz <anajuliathomaz85@gmail.com>
2025-06-17 11:40:20 -0300

    reltorio

--- conexao.php
+++ conexao.php
@@ -1,13 +1,14 @@
 <?php
-$host = 'localhost'; // ou o endereço do seu servidor de banco de dados
+$host = 'localhost';
 $dbname = 'biblioteca';
-$username = 'root'; // seu usuário do banco de dados
-$password = ''; // sua senha do banco de dados
+$username = 'root';
+$password = '';
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo "Erro de conexão: " . $e->getMessage();
+    exit;
 }
 ?>
